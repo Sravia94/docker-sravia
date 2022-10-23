33fa8f0a5e05e535c4cd810a24ae26159134b7c3
Sravia94 <104487923+Sravia94@users.noreply.github.com>
2022-10-23 20:01:29 +0530

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -1,6 +1,5 @@
-sudo apt-get update
-sudo apt install -y curl jq
-sudo curl https://get.docker.com | sudo bash
+
+curl https://get.docker.com | sudo bash
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
